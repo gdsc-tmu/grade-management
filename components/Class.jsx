c12378009cca40f5a68798141741c9fa432a2d07
bbtit <the8butterfly8dream@gmail.com>
2022-12-14 15:18:19 +0900

    First Commit

---
+++ components/Class.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+
+export default function Class(props) {
+  return (
+    
+    <div>
+      <Link href={props.url}>
+        {props.name}
+      </Link>
+      <div>{props.code}</div>
+      <div>{props.teacher}</div>
+      <div>{props.type}</div>
+      <div>{props.credit}</div>
+      <div>{props.year}</div>
+      <div>{props.term}</div>
+      <div>{props.day}</div>
+      <div>{props.period}</div>
+    </div>
+  );
+}
---
+++ components/ClassBar.jsx
@@ -0,0 +1,59 @@
+import { useCallback, useEffect, useState } from "react";
+import Class from "./Class";
+
+export default function() {
+  const [text, setText] = useState("");
+  const [classes, setClasses] = useState([]);
+  const [classesInfo, setClassesInfo] = useState([]);
+  const [classGroup, setClassGroup] = useState({});
+  
+
+  const handleChange = useCallback(e => {
+    setText(e.target.value);
+  }, []);
+
+  const handleClick = () => {
+    // ToDo 重複・空欄・英小文字・fetch errorの処理追加
+    setClasses(classes => [...classes,text]);
+    fetch("https://tmu-syllabus-default-rtdb.firebaseio.com/2022/"+ text + ".json")
+      .then((res) => res.json())
+      .then((data) => setClassesInfo([...classesInfo,data]));
+    setText("");
+  };
+
+  useEffect(()=>{
+    // ToDo グラフ表示用にclassInfoを集計してclassGroupに代入 → グラフコンポーネントにデータを渡す
+    console.log("in useEffect")
+  },[classesInfo]);
+
+  return (
+    <div>
+      <label htmlFor="code">授業コード : </label>
+      <input type="text" value={text} id="code" name="code" onChange={handleChange}></input>
+      <button onClick={handleClick}>決定</button>
+
+      <div>{classes}</div>
+
+      {classesInfo.map((classinfo,index) => {
+        return (
+          <Class 
+            key={classinfo.code} 
+            name={classinfo.name} 
+            code={classinfo.code} 
+            url={classinfo.url} 
+            teacher={classinfo.teacher} 
+            type={classinfo.type} 
+            credit={classinfo.credit} 
+            year={classinfo.year} 
+            term={classinfo.term} 
+            day={classinfo.day} 
+            period={classinfo.period} 
+            method={setClassesInfo} 
+            index={index} 
+            />
+        );
+      })}
+
+    </div>
+  );
+}
