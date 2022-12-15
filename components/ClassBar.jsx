1dbb97a8a9b88f13d99a394ded0dc2c790ca92b4
bbtit <the8butterfly8dream@gmail.com>
2022-12-16 01:39:40 +0900

    [style]入力フォームの調整

--- components/ClassBar.jsx
+++ components/ClassBar.jsx
@@ -62,7 +62,28 @@ export default function ClassBar() {
 
   return (
     <div className="container mx-auto my-4 px-4">
-      <Example/>
+      <div className="w-80 mx-auto">
+        <label htmlFor="code" className="block text-md font-medium text-gray-800">
+          授業コード
+        </label>
+        <div className="flex items-center">
+          <div className="relative mt-1 rounded-md shadow-sm">
+            <input type="text" name="code" id="code" className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="A0000"/>
+            <div className="absolute inset-y-0 right-0 flex items-center">
+              <label htmlFor="select-year" className="sr-only">
+                select-year
+              </label>
+              <select id="select-year" name="select-year" className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                <option>2022</option>
+                <option>2021</option>
+              </select>
+            </div>
+          </div>
+          <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded-lg">決定</button>
+        </div>
+      </div>
+
+
       <label htmlFor="code">授業コード : </label>
       <input type="text" value={text} id="code" name="code" placeholder="授業コードを入力" onChange={handleChange} className=""></input>
       <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">決定</button>
