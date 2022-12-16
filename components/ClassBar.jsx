80cc6950766a27f606f7dd2b1430f87d4ef7bb58
bbtit <the8butterfly8dream@gmail.com>
2022-12-16 11:59:14 +0900

    [style]Header.jsxの追加

--- components/ClassBar.jsx
+++ components/ClassBar.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import { ChartBar } from "./Chart";
 import Class from "./Class";
 import Example from "./Example";
+import Header from "./Header";
 
 export default function ClassBar() {
   const [text, setText] = useState("");
@@ -15,7 +16,7 @@ export default function ClassBar() {
   }, []);
 
   const handleClick = () => {
-
+    // ToDo 選択した年度のデータの取得・処理
     const moldedText = text.trim().toUpperCase();
 
     if (classes.includes(moldedText)){
@@ -61,57 +62,55 @@ export default function ClassBar() {
   }
 
   return (
-    <div className="container mx-auto my-4 px-4">
-      <div className="w-80 mx-auto">
-        <label htmlFor="code" className="block text-md font-medium text-gray-800">
-          授業コード
-        </label>
-        <div className="flex items-center">
-          <div className="relative mt-1 rounded-md shadow-sm">
-            <input type="text" name="code" id="code" className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="A0000"/>
-            <div className="absolute inset-y-0 right-0 flex items-center">
-              <label htmlFor="select-year" className="sr-only">
-                select-year
-              </label>
-              <select id="select-year" name="select-year" className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                <option>2022</option>
-                <option>2021</option>
-              </select>
+    <>
+      <Header/>
+      <div className="container mx-auto my-4 px-4">
+        <div className="w-80 mx-auto">
+          <label htmlFor="code" className="block text-md font-medium text-gray-800">
+            授業コード
+          </label>
+          <div className="flex items-center">
+            <div className="relative mt-1 rounded-md shadow-sm">
+              <input type="text" value={text} onChange={handleChange} name="code" id="code" className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="A0000"/>
+              <div className="absolute inset-y-0 right-0 flex items-center">
+                <label htmlFor="select-year" className="sr-only">
+                  select-year
+                </label>
+                <select id="select-year" name="select-year" className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                  <option>2022</option>
+                  <option>2021</option>
+                </select>
+              </div>
             </div>
+            <button onClick={handleClick} className="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 ml-2 rounded-lg">決定</button>
           </div>
-          <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded-lg">決定</button>
         </div>
+
+        <div>{classes}</div>
+
+        {classesInfo.map((classinfo,index) => {
+          return (
+            <Class 
+              key={classinfo.code} 
+              name={classinfo.name} 
+              code={classinfo.code} 
+              url={classinfo.url} 
+              teacher={classinfo.teacher} 
+              type={classinfo.type} 
+              credit={classinfo.credit} 
+              year={classinfo.year} 
+              term={classinfo.term} 
+              day={classinfo.day} 
+              period={classinfo.period} 
+              deleteMethod={deleteClass} 
+              index={index} 
+              />
+          );
+        })}
+        <ChartBar label={["取得単位数","必要単位数"]} data1={[20,12]} data2={[28,28]}/>
+        <ChartBar label={["取得単位数","必要単位数"]} data1={[20,12]} data2={[28,28]}/>
+        <ChartBar label={["取得単位数","必要単位数"]} data1={[20,12]} data2={[28,28]}/>
       </div>
-
-
-      <label htmlFor="code">授業コード : </label>
-      <input type="text" value={text} id="code" name="code" placeholder="授業コードを入力" onChange={handleChange} className=""></input>
-      <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">決定</button>
-
-      <div>{classes}</div>
-
-      {classesInfo.map((classinfo,index) => {
-        return (
-          <Class 
-            key={classinfo.code} 
-            name={classinfo.name} 
-            code={classinfo.code} 
-            url={classinfo.url} 
-            teacher={classinfo.teacher} 
-            type={classinfo.type} 
-            credit={classinfo.credit} 
-            year={classinfo.year} 
-            term={classinfo.term} 
-            day={classinfo.day} 
-            period={classinfo.period} 
-            deleteMethod={deleteClass} 
-            index={index} 
-            />
-        );
-      })}
-      <ChartBar label={["取得単位数","必要単位数"]} data1={[20,12]} data2={[28,28]}/>
-      <ChartBar label={["取得単位数","必要単位数"]} data1={[20,12]} data2={[28,28]}/>
-      <ChartBar label={["取得単位数","必要単位数"]} data1={[20,12]} data2={[28,28]}/>
-    </div>
+    </>
   );
 }
--- components/Example.jsx
+++ components/Example.jsx
@@ -20,22 +20,12 @@ export default function Example() {
         授業コード
       </label>
       <div className="relative mt-1 rounded-md shadow-sm">
-        <input
-          type="text"
-          name="code"
-          id="code"
-          className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          placeholder="A0000"
-        />
+        <input type="text" name="code" id="code" className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="A0000"/>
         <div className="absolute inset-y-0 right-0 flex items-center">
           <label htmlFor="select-year" className="sr-only">
             select-year
           </label>
-          <select
-            id="select-year"
-            name="select-year"
-            className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          >
+          <select id="select-year" name="select-year" className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
             <option>2022</option>
             <option>2021</option>
           </select>
